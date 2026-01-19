7723f48a1602c0189482608efd1ddc40d5ba9720
ttkwzyttk <1351930016@qq.com>
2026-01-19 17:55:59 +0800

    hotfix:     - 恢复测试代码

--- src/components/common/comBefore.js
+++ src/components/common/comBefore.js
@@ -22,12 +22,8 @@ export default function main(_) {
      * 国家公祭日和自定义缅怀的日期
      */
     const today = _.__tools.getFormattedDate();
-    if (today === '1-19' || _.__config.memorialDays.includes(today)) {
-        $('html').css('filter', 'grayscale(100%)');
-        console.log('纪念日样式生效');
-    }
-    console.log('纪念日样式未生效');
-    
+    if (today === '12-13' || _.__config.memorialDays.includes(today)) $('html').css('filter', 'grayscale(100%)');
+
     let loadingObj = loading(_);
 
     /**
