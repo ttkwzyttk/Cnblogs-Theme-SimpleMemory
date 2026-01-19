f4ece41c6a903efa6d36972fd8a1092ff0c206b3
ttkwzyttk <1351930016@qq.com>
2026-01-19 20:56:48 +0800

    hotfix:     - 修改slider宽度

--- src/components/event/event.js
+++ src/components/event/event.js
@@ -19,10 +19,16 @@ export default function main(_) {
             _.__event.scroll.temScroll  = 0; // 上一次页面滚动位置
             _.__event.scroll.docScroll  = $(document).scrollTop(); // 当前滚动位置
             _.__event.scroll.homeScroll = $('#home').offset().top - 40; // 主体滚动
+
+            // 当发生窗口滚动时，执行以下代码
             $(window).scroll(() => {
+                // 更新当前滚动位置数据
                 _.__event.scroll.docScroll  = $(document).scrollTop();
+                // 更新主页位置数据
                 _.__event.scroll.homeScroll = $('#home').offset().top - 40;
+                // 执行滚动处理函数
                 eventFun.handle.scroll();
+                // 更新上一次滚动位置数据
                 _.__event.scroll.temScroll = _.__event.scroll.docScroll;
             });
 
@@ -31,6 +37,8 @@ export default function main(_) {
              */
             _.__event.resize = {};
             _.__event.resize.handle = [];
+
+            // 当发生窗口大小变化时，执行以下代码
             $(window).resize( () => {
                 eventFun.handle.resize();
             });
@@ -46,6 +54,7 @@ export default function main(_) {
                 for (let i = 0; i < _.__event.resize.handle.length; i++) {
                     (_.__event.resize.handle[i])();
                 }
+                // 目的就是为了保证主页内容和头部的对齐，响应式布局中头部高度变化时及时更新主页位置
                 _.__tools.setDomHomePosition();
             },
         }
